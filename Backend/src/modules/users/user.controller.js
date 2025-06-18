602c769a436f13f2c986ffb3f15197b81e16cdab
Mary Laura Carolina Limay Oliva <limayolivamary@gmail.com>
2025-06-18 11:52:26 -0500

    cambios en controller

--- Backend/src/modules/users/user.controller.js
+++ Backend/src/modules/users/user.controller.js
@@ -1,62 +1,66 @@
-import { UsuarioService } from './user.service.js';
+import { UsuarioService } from "./user.service.js";
 
-export const crearUsuarioController = async (req, res) => {
-  try {
-    const nuevoUsuario = await UsuarioService.crearUsuario(req.body);
-    return res.status(201).json(nuevoUsuario);
-  } catch (error) {
-    console.error('Error al crear usuario:', error);
-    return res.status(500).json({ error: 'Error del servidor' });
-  }
-};
+export const UsuarioController = {
+  async crearUsuario(req, res) {
+    try {
+      const nuevoUsuario = await UsuarioService.crearUsuario(req.body);
+      return res.status(201).json(nuevoUsuario);
+    } catch (error) {
+      console.error("Error al crear usuario:", error);
+      return res.status(500).json({ error: "Error del servidor" });
+    }
+  },
 
-export const obtenerUsuariosController = async (req, res) => {
-  // cuando se brinda un id en params
-
+  async obtenerUsuarios(req, res) {
     try {
       const usuarios = await UsuarioService.obtenerUsuarios();
       return res.status(200).json(usuarios);
     } catch (error) {
-      console.error('Error al obtener usuarios:', error);
-      return res.status(500).json({ error: 'Error del servidor' });
+      console.error("Error al obtener usuarios:", error);
+      return res.status(500).json({ error: "Error del servidor" });
     }
-};
- export const obtenerUsuarioPorIdController = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const usuario = await UsuarioService.obtenerUsuarioPorId(id);
-    if (!usuario) {
-      return res.status(404).json({ error: 'Usuario no encontrado' });
+  },
+  async obtenerUsuarioPorId(req, res) {
+    try {
+      const { id } = req.params;
+      const usuario = await UsuarioService.obtenerUsuarioPorId(id);
+      if (!usuario) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+      return res.status(200).json(usuario);
+    } catch (error) {
+      console.error("Error al obtener usuario por ID:", error);
+      return res.status(500).json({ error: "Error del servidor" });
     }
-    return res.status(200).json(usuario);
-  } catch (error) {
-    console.error('Error al obtener usuario por ID:', error);
-    return res.status(500).json({ error: 'Error del servidor' });
-  }
-};
-export const actualizarUsuarioController = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const usuarioActualizado = await UsuarioService.actualizarUsuarioPorId(id, req.body);
-    if (!usuarioActualizado) {
-      return res.status(404).json({ error: 'Usuario no encontrado' });
+  },
+
+  async actualizarUsuarioPorId(req, res) {
+    try {
+      const { id } = req.params;
+      const usuarioActualizado = await UsuarioService.actualizarUsuarioPorId(
+        id,
+        req.body
+      );
+      if (!usuarioActualizado) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+      return res.status(200).json(usuarioActualizado);
+    } catch (error) {
+      console.error("Error al actualizar usuario:", error);
+      return res.status(500).json({ error: "Error del servidor" });
     }
-    return res.status(200).json(usuarioActualizado);
-  } catch (error) {
-    console.error('Error al actualizar usuario:', error);
-    return res.status(500).json({ error: 'Error del servidor' });
-  }
-};
-export const eliminarUsuarioController = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const resultado = await UsuarioService.eliminarUsuarioPorId(id);
-    if (!resultado) {
-      return res.status(404).json({ error: 'Usuario no encontrado' });
+  },
+  async eliminarUsuarioPorId(req, res) {
+    try {
+      const { id } = req.params;
+      const resultado = await UsuarioService.eliminarUsuarioPorId(id);
+      if (!resultado) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+      return res.status(204).send();
+    } catch (error) {
+      console.error("Error al eliminar usuario:", error);
+      return res.status(500).json({ error: "Error del servidor" });
     }
-    return res.status(204).send();
-  } catch (error) {
-    console.error('Error al eliminar usuario:', error);
-    return res.status(500).json({ error: 'Error del servidor' });
   }
 };
--- Backend/src/modules/users/user.routes.js
+++ Backend/src/modules/users/user.routes.js
@@ -1,16 +1,11 @@
 import { Router } from "express";
 import {  crearUsuarioSchema, actualizarUsuarioSchema } from "./user.schema.js";
 import { validateSchema } from "../../middlewares/validateSchema.js";
-import { crearUsuarioController, 
-    obtenerUsuarioPorIdController, 
-    obtenerUsuariosController, 
-    actualizarUsuarioController,
-    eliminarUsuarioController
-} from "./user.controller.js";
+import { UsuarioController } from "./user.controller.js";
 export const userRoutes = Router();
 
-userRoutes.post("/", validateSchema(crearUsuarioSchema), crearUsuarioController);
-userRoutes.get("/", obtenerUsuariosController);
-userRoutes.get("/:id", obtenerUsuarioPorIdController); 
-userRoutes.patch("/:id", validateSchema(actualizarUsuarioSchema), actualizarUsuarioController); 
-userRoutes.delete("/:id", eliminarUsuarioController);
+userRoutes.post("/", validateSchema(crearUsuarioSchema), UsuarioController.crearUsuario);
+userRoutes.get("/", UsuarioController.obtenerUsuarios);
+userRoutes.get("/:id", UsuarioController.obtenerUsuarioPorId);
+userRoutes.patch("/:id", validateSchema(actualizarUsuarioSchema), UsuarioController.actualizarUsuarioPorId);
+userRoutes.delete("/:id", UsuarioController.eliminarUsuarioPorId);
